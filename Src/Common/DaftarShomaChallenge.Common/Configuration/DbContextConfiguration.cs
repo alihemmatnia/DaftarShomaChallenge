c874110211cfe97fd92519219590d640ca004824
alihemmatnia <ali.hemmatnia@gmail.com>
2024-06-18 21:46:07 +0330

    clean project

---
+++ Src/Common/DaftarShomaChallenge.Common/Configuration/DbContextConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace DaftarShomaChallenge.Common.Configuration
+{
+	public class DbContextConfiguration
+	{
+		public const string SECTION_NAME = "DbContext";
+
+		public string ConnectionString { get; set; }
+		public static DbContextConfiguration Config { get; set; }
+
+		public static DbContextConfiguration FromConfiguration (IConfiguration configuration)
+		{
+			IConfigurationSection section;
+			try
+			{
+				section = configuration.GetRequiredSection(SECTION_NAME);
+			}
+			catch (InvalidOperationException)
+			{
+				throw new Exception(SECTION_NAME);
+			}
+
+			try
+			{
+				var config = new DbContextConfiguration();
+				new ConfigureFromConfigurationOptions<DbContextConfiguration>(section)
+				   .Configure(config);
+				return config;
+			}
+			catch (Exception)
+			{
+				throw new Exception(SECTION_NAME);
+			}
+		}
+	}
+}
