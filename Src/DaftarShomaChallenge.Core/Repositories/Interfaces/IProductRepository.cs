3a00a7b4c3fd6a3b5b258d793b039ea8fd77dea0
alihemmatnia <ali.hemmatnia@gmail.com>
2024-06-21 18:31:59 +0330

    fix bug

--- Src/DaftarShomaChallenge.Core/Repositories/Interfaces/IProductRepository.cs
+++ Src/DaftarShomaChallenge.Core/Repositories/Interfaces/IProductRepository.cs
@@ -7,7 +7,6 @@ namespace DaftarShomaChallenge.Core.Repositories.Interfaces
 	{
 		Task<bool> CreateProduct (Product product, CancellationToken cancellationToken);
 		Task<bool> RemoveProduct (Product product, CancellationToken cancellationToken);
-		Task UpdateProduct (Product product);
 		Task<Product> GetOne (int productId, CancellationToken cancellationToken);
 		Task<List<Product>> GetAll (List<int> productIds, CancellationToken cancellationToken);
 		Task<PageableResponse<Product>> GetPageable(Pageable pageable, CancellationToken cancellationToken);
